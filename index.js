24701aac3f62436817f8549160240ea3aac94d14
Eugenio <euge060406@gmail.com>
2023-10-01 23:15:48 -0300

    commit 10

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.listen(4000)
 
 
 app.use(cors({ origin: 'http://localhost:5173' }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
