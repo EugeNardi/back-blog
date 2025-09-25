24872f25c1533612dbe53d7178ad88647b9b4294
EugeNardi <euge060406@gmail.com>
2025-09-25 12:26:44 -0300

    2025/09

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ const secret = 'asdfe45we45w345wegw345werjktjwertkjasbfoafnqwojfbqwijfm13rboj12r
 app.listen(4000)
 
 const corsOptions = {
-  origin: 'http://localhost:5173/',
+  origin: 'https://back-blog-beta.vercel.app/',
   credentials: true,
 };
 app.use(cors(corsOptions));
 
-app.use(cors({ origin: 'http://localhost:5173/', credentials:true }));
+app.use(cors({ origin: 'https://back-blog-beta.vercel.app/', credentials:true }));
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://back-blog-beta.vercel.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
