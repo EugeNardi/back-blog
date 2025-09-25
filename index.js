fba446d3c9501ed7ab629105ad4f6bdd46c60289
EugeNardi <euge060406@gmail.com>
2025-09-25 12:48:52 -0300

    commit 33

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ const secret = 'asdfe45we45w345wegw345werjktjwertkjasbfoafnqwojfbqwijfm13rboj12r
 app.listen(4000)
 
 const corsOptions = {
-  origin: 'https://back-blog-beta.vercel.app/',
+  origin: 'https://noticias-x.netlify.app/',
   credentials: true,
 };
 app.use(cors(corsOptions));
 
-app.use(cors({ origin: 'https://back-blog-beta.vercel.app/', credentials:true }));
+app.use(cors({ origin: 'https://noticias-x.netlify.app/', credentials:true }));
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://back-blog-beta.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://noticias-x.netlify.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
